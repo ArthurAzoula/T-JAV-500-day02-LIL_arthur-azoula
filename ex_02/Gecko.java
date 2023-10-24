3376973795b37fcda35db6d14623b6595b0e11b3
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-24 09:59:51 +0200

    Add exo2

---
+++ ex_02/Gecko.java
@@ -0,0 +1,14 @@
+public class Gecko {
+
+    public String name = "Unknown";
+    public Gecko() {
+
+        System.out.println("Hello!");
+
+    }
+    public Gecko(String name) {
+        this.name = name;
+        System.out.println("Hello " + name + "!");
+    }
+
+}
