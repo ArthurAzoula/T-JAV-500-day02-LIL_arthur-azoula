a8747a33fb0d95484f823af06f8f6cf9dee4e409
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-24 10:57:27 +0200

    add task06

---
+++ ex_06/Gecko.java
@@ -0,0 +1,92 @@
+public class Gecko {
+
+    private String name = "Unknown";
+    privat int energy = 100;
+    private int age;
+
+    public Gecko() {
+
+        System.out.println("Hello!");
+
+    }
+
+    public Gecko(String name) {
+        this.name = name;
+        System.out.println("Hello " + name + "!");
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getEnergy() {
+        return this.energy;
+    }
+
+    public void setEnergy(int energy) {
+        if (this.energy + energy <= 100 && this.energy + energy >= 0) {
+            this.energy + energy = 100;
+        }
+    }
+
+    public void status() {
+        switch (this.age) {
+            case 0:
+                System.out.println("Unborn Gecko");
+                break;
+            case 1:
+            case 2:
+                System.out.println("Baby Gecko");
+                break;
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+                System.out.println("Adult Gecko");
+                break;
+            case 11:
+            case 12:
+            case 13:
+                System.out.println("Old Gecko");
+                break;
+            default:
+                System.out.println("Impossible Gecko");
+                break;
+        }
+    }
+
+    public void hello(string name) {
+        System.out.println("Hello " + name + ", I'm " + this.name + "!");
+    }
+
+    public void hello(int name) {
+        for (int i = 0; i < name; i++) {
+            System.out.println("Hello, I'm " + this.name + "!");
+        }
+    }
+
+    public void eat(String food) {
+        if (food.equals("Meat")) {
+            setEnergy(-10);
+            System.out.println("Yummy!");
+        } else if (food.equals("Vegetable")) {
+            setEnergy(-10);
+            System.out.println("Erk!");
+        } else {
+            System.out.println("I can't eat this!");
+        }
+    }
+
+}
